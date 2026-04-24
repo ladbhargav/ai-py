7a36082daac3b013c2959bbb2e46fa852fcaf280
ladbhargav <ladbhargav1111@gmail.com>
2026-04-24 10:38:30 +0000

    done

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
 from config.llm import llm
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def Hello():
     return {"hello":"world"}
 
+class ProjectInput(BaseModel):
+    text: str
+
 @app.post("/suggest")
-def suggest(text:str):
+def suggest(data:ProjectInput):
     messages = [
         (
             "system",
@@ -22,7 +36,7 @@ def suggest(text:str):
             - Use Title Case
             - If the name is already correct, return it as-is"""
         ),
-        ("human", text)
+        ("human", data.text)
     ]
     result = llm.invoke(messages)
-    return result.content
+    return {"msg":result.content}
